f25352b779a779cd386eb1ace5a2a45a7a606b36
mtallerico1 <mtallerico1@gmail.com>
2018-04-06 07:50:27 -0400

    Working through Javascript for pagination.

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-const studentList = document.querySelector('ul')
+const studentList = document.querySelector('ul');
 const students = document.querySelectorAll('li');
-let pageCount = 1;
+const page = document.querySelector('.page');
+const paginationList = document.querySelector('.pagination')
+const div = document.createElement('div');
+const li = document.createElement('li');
+const ul = document.createElement('ul');
 
-for (i = 0; i <= students.length; i++) {
-    if (i >= 10) {
-        students[i].style.display = "none";
+
+
+// document.addEventListener('load', showPage(1, students));
+document.addEventListener('load', appendPageLinks(students));
+
+
+
+// function showPage(pageNumber, list) {
+//     for (let i = 0; i <= list.length; i++) {
+//         list[i].style.display = "none";
+//     }
+// }
+
+function appendPageLinks(list) {
+    
+    let pages = Math.ceil(list.length / 10);
+    if (students.length > 10) {
+        page.appendChild(ul);
+        ul.setAttribute('class', 'pagination');
+        const paginationList = document.querySelector('.pagination');
+        paginationList.appendChild(li);
+        li.textContent = pages;
+        for (let i = 1; i <= pages.length; i++) {
+            
+        }
     }
 }
 
