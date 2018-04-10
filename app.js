1e1c95fb67edf12c56296156beb895957144646c
mtallerico1 <mtallerico1@gmail.com>
2018-04-10 07:29:18 -0400

    Creating funtion to display only list itmes that belong on page.

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 const studentList = document.querySelector('.student-list');
-const students = document.querySelectorAll('li');
+const students = document.querySelectorAll('.student-item');
 const page = document.querySelector('.page');
-const div = document.createElement('div');
+let pages = Math.ceil(students.length / 10);
 
 
 
 
-// document.addEventListener('load', showPage(1, students));
+document.addEventListener('load', showPage(1, students));
 document.addEventListener('load', appendPageLinks(students));
 
 
 
-// function showPage(pageNumber, list) {
-//     for (let i = 0; i <= list.length; i++) {
-//         list[i].style.display = "none";
-//     }
-// }
+function showPage(pageNumber, list) {
+    let maxList = (pageNumber * 10) - 1;
+    let minList = (pageNumber * 10) - 10;
+    for (let i = 0; i < list.length; i++) {
+        list[i].style.display = 'none';
+        if ((list[i] >= minList) && (list[i] <= maxList) ) {
+            list[i].removeAttribute(style);
+            console.log('yes');
+        } 
+    } 
+}
+
+
 
 function appendPageLinks(list) {
     const ul = document.createElement('ul');
-    let pages = Math.ceil(list.length / 10);
     page.appendChild(ul).setAttribute('class', 'pagination');
     const paginationList = document.querySelector('.pagination')
     for (let i = 1; i <= pages; i++) {
         let pageNum = i;
         const li = document.createElement('li');
         const a = document.createElement('a');
-        paginationList.appendChild(li).appendChild(a);
-        let linkText
+        const link = paginationList.appendChild(li).appendChild(a);
+        link.innerText = i;
     }
 }
 
